3ec75a785f34b554b67ee395e5f05467b0d8c332
Ziqiang Li <ziqiang.lee@gmail.com>
2020-12-11 16:57:13 +0800

    fix

--- src/build.js
+++ src/build.js
@@ -1,5 +1,5 @@
 const alfy = require('alfy');
-const UTIL = requir('./util');
+const UTIL = require('./util');
 const jenkins = require('./jenkins');
 const DELAY = UTIL.getEnv('DELAY') || 0;
 jenkins.job.build(alfy.input, { delay: `${DELAY}sec`});
