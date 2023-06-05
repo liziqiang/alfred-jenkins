1214b58f73f34f66c75dee5b45efcdecd3e9f49f
Ziqiang Li <ziqiang.lee@gmail.com>
2023-06-05 13:52:04 +0800

    feat: 🎸 更新 iocn

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function outputJobs(jobs) {
                 }
             },
             icon: {
-                path: UTIL.resolvePath(`./images/${job.color}.png`)
+                path: UTIL.resolvePath(`./images/${job.color}_anime.gif`)
             }
         };
     });
